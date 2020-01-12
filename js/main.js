e50f695f8a8bc6f1c7368fc273dd6e4413432192
zDridi <dridi.zied@courrier.uqam.ca>
2020-01-12 15:02:15 -0500

    9 eme commit : modification de Time zone

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 // Set the date we're counting down to
-// var countDownDate = new Date("Feb 30, 2020 00:00:00").getTime();
-var countDownDate = new Date("2020-06-30T00:00:00Z").getTime();
+var countDownDate = new Date("2020-06-30T00:-04:00").getTime(); // New-York time zone
 
   // Update the count down every 1 second
   var x = setInterval(function() {
