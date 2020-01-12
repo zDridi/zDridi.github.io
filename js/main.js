70f6a35f9f8aa7f65e2e7e836ea0fe574adab8f9
zDridi <dridi.zied@courrier.uqam.ca>
2020-01-12 14:47:54 -0500

    8 eme commit : modification de la date de diponibilité du site web

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Feb 30, 2020 00:00:00").getTime();
+// var countDownDate = new Date("Feb 30, 2020 00:00:00").getTime();
+var countDownDate = new Date("2020-06-30T00:00:00Z").getTime();
 
   // Update the count down every 1 second
   var x = setInterval(function() {
