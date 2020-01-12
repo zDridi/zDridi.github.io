40056f671c7e3a5ad4f28a602417cd930c51785a
zDridi <dridi.zied@courrier.uqam.ca>
2020-01-12 15:19:36 -0500

    10 eme commit : Time zone : bogue corrigé

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("2020-06-30T00:-04:00").getTime(); // New-York time zone
+var countDownDate = new Date("2020-06-30T05:00:00Z").getTime();
 
   // Update the count down every 1 second
   var x = setInterval(function() {
