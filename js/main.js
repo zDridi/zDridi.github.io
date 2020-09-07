694af5ff84a16aefdd789b554a438e4b52fffd41
Zied Dridi <dridi.zied@courrier.uqam.ca>
2020-09-07 15:53:37 -0400

    mise à jour de la date de mise en prod

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("2020-06-30T05:00:00Z").getTime();
+var countDownDate = new Date("2020-12-31T05:00:00Z").getTime();
 
   // Update the count down every 1 second
   var x = setInterval(function() {
